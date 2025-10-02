85a620c9f2c2aede783e6d2abb8527f0824b62fe
Катасонов Андрей Сергеевич ИП-515 <andrewkatassonov@mail.ru>
2025-10-02 09:44:35 +0700

    Исправлено приветствие

--- main.cpp
+++ main.cpp
@@ -4,7 +4,13 @@
 int main(int argc, char** argv) {
  char* str = (char*)malloc(sizeof(char) * SIZE);
  printf("Катасонов Андрей Сергеевич:\n");
+
  str = gets(str);
  printf("Привет, %s", str);
+
+ scanf("%s", str);
+ puts("Привет");
+ puts(str);
+ ee364fb (Исправлено приветствие)
  return 0;
 }
